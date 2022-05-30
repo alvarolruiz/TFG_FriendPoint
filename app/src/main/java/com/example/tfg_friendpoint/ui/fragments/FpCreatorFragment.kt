ef6db7948d4b6a891af9290bfaf680aff1052aa9
alvarolruiz <alvarolaurino@gmail.com>
2022-05-31 01:39:07 +0200

    nav component updated with details and create fragment

--- app/src/main/java/com/example/tfg_friendpoint/ui/fragments/FpCreatorFragment.kt
+++ app/src/main/java/com/example/tfg_friendpoint/ui/fragments/FpCreatorFragment.kt
@@ -6,16 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tfg_friendpoint.R
-import com.example.tfg_friendpoint.databinding.FragmentChatBinding
+import com.example.tfg_friendpoint.databinding.FragmentFpCreatorBinding
 
-class ChatFragment : Fragment() {
-private lateinit var mBinding : FragmentChatBinding
+class FpCreatorFragment : Fragment() {
+    private lateinit var mBinding: FragmentFpCreatorBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        mBinding = FragmentChatBinding.inflate(inflater,container,false)
+        mBinding = FragmentFpCreatorBinding.inflate(inflater,container,false)
         val view = mBinding.root
         return view
     }
--- app/src/main/java/com/example/tfg_friendpoint/ui/fragments/FriendPointFragment.kt
+++ app/src/main/java/com/example/tfg_friendpoint/ui/fragments/FriendPointFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tfg_friendpoint.R
+import com.example.tfg_friendpoint.databinding.FragmentFriendPointBinding
 
 class FriendPointFragment : Fragment() {
-
+private lateinit var mBinding : FragmentFriendPointBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend_point, container, false)
+        mBinding =  FragmentFriendPointBinding.inflate(inflater, container,false)
+        val view = mBinding.root
+        return view
     }
 
 }
